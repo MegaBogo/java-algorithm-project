7a2c48a6684ec9b4b673d109fe994bf85c96212d
dbogo <devbogo@gmail.com>
2019-03-24 00:17:51 +0900

    5-7

--- src/doIt/chap01/Ex01_5.java
+++ src/doIt/chap01/Ex01_5.java
@@ -26,6 +26,32 @@ public class Ex01_5 {
 		
 		System.out.println("합 : "+sum);
 		
+		/*
+		 * 사전 판단 반복과 사후 판단 반복 차이
+		 * 
+		 * 사전 판단 : while,for문
+		 * 결과가 0이면 루프 본문은 한 번도 실행되지 않음.
+		 * 
+		 * 사후 판단 : do문
+		 * 루프 본문은 반드시 한 번은 실행.
+		 */
+		
+		/*
+		 * a,b 정수를 받고 b-a를 출력하는 프로그램
+		 * 단, b는 a보다 큰수
+		 */
+		System.out.println("A를 입력하세요 : ");
+		int a = stdIn.nextInt();		
+		int b;
+		
+		do {
+			System.out.println("B를 입력하세요 : ");
+			b = stdIn.nextInt(); 
+		} while (a>b);
+		
+		System.out.println("a-b는 ?"+(b-a));
+		
+		
 
 	}
 
---
+++ src/doIt/chap01/Ex01_7.java
@@ -0,0 +1,17 @@
+package doIt.chap01;
+
+
+public class Ex01_7 {
+
+	public static void main(String[] args) {
+		
+		
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j<=9; j++) {
+				System.out.printf("%3d", i * j);
+			}
+			System.out.println();
+		}
+		
+	}
+}
